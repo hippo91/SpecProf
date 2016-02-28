d02872abce7a16f1f6986c5b11255c3f45ed02c8
hippo91 <guillaume.peillex@gmail.com>
2016-02-28 21:16:47 +0100

    Creation of private methods to extract return type and parameters of a function prototype

--- src/function_wrapper_writer.py
+++ src/function_wrapper_writer.py
@@ -6,18 +6,21 @@ import logging
 import subprocess
 import os
 import jinja2
+import re
 
 logger = logging.getLogger(__name__)
 
+PATH_TO_TEMPLATES = os.path.join(os.path.dirname(__file__), "jinja_templates")
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(PATH_TO_TEMPLATES),
+                                       extensions=['jinja2.ext.autoescape'],
+                                       autoescape=True)
+FUNC_PROTO_PATTERN = "^\s*(\w+\s*\**)\s*(\w+)\s*\(.*\)"
+FUNC_PROTO_PO = re.compile(FUNC_PROTO_PATTERN)
+
 class FunctionWrapperWriter(object):
     """
-    A class that creates a c file that wrapps the call to a specific function
+    A class that creates a c file that wrapps the call to a specific function inside a shared object
     """
-    path_to_templates = os.path.join(os.path.dirname(__file__), "jinja_templates")
-    jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(path_to_templates),
-                                           extensions=['jinja2.ext.autoescape'],
-                                           autoescape=True)
-    
     def __init__(self, function_symbol):
         """
         :param function_symbol: symbol of the function to wrap
@@ -29,7 +32,7 @@ class FunctionWrapperWriter(object):
         """
         Write c file wrapper using jinja and template
         """
-        template = self.jinja_environment.get_template('template_cfile.c')
+        template = JINJA_ENVIRONMENT.get_template('template_cfile.c')
         opt_includes = ["""/home/guillaume2/Documents/Informatique/WORKSPACE_ECLIPSE/"""
                         """nonlinear_solver/Newton/miegruneisen.h"""]
         target_library = ("""/home/guillaume2/Documents/Informatique/WORKSPACE_ECLIPSE/XVOF_NP/xvof/"""
@@ -43,6 +46,45 @@ class FunctionWrapperWriter(object):
                            'target_symbol': target_symbol}
         print template.render(template_values)
 
+    @staticmethod
+    def _getFunctionReturnType(func_prototype):
+        """
+        Return the return type of the function which prototype is func_prototype
+        
+        :param func_prototype: prototype of the function
+        :type func_prototype: str
+        :return: return type of the function
+        :rtype: str
+        """
+        results = re.search(FUNC_PROTO_PO, func_prototype).groups()
+        return results[0]
+        
+    @staticmethod
+    def _getFunctionParameters(func_prototype):
+        """
+        Return the parameters of the function which prototype is func_prototype
+        
+        :param func_prototype: prototype of the function
+        :type func_prototype: str
+        :return: parameters of the function in the form : (type name, type name ...)
+        :rtype: str  
+        """
+        results = re.search(FUNC_PROTO_PO, func_prototype).groups()
+        return results[2]
+        
+    @staticmethod
+    def _getFunctionParametersNames(func_parameters):
+        """
+        :param func_parameters: parameters of the function as return by the method _getParameters
+        :type func_parmaters: str
+        :return: name of the parameters of the function
+        :rtype: list
+        """
+
 if __name__ == "__main__":
+    target_signature = ("""void solveVolumeEnergy(MieGruneisenParameters_t *params, const double specific_volume,"""
+                        """ const double internal_energy, double* pressure, double* gamma_per_vol, double* c_son)""")
     my_func_wrap = FunctionWrapperWriter(None)
     my_func_wrap.writeCFile("toto", "tata")
+    print my_func_wrap._getFunctionReturnType(target_signature)
+#     my_func_wrap._getFunctionParameters(target_signature)
