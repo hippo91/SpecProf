39522bdd7645e146a595810903e1136a234f9d5f
hippo91 <guillaume.peillex@gmail.com>
2016-03-06 15:49:33 +0100

    Adding compileSrcFile method

--- src/function_wrapper_writer.py
+++ src/function_wrapper_writer.py
@@ -7,6 +7,7 @@ import subprocess
 import os
 import jinja2
 import re
+from sys import stderr
 
 logger = logging.getLogger(__name__)
 
@@ -21,16 +22,34 @@ FUNC_PARAMETER_PO = re.compile(FUNC_PARAMETER_PATTERN)
 
 class FunctionWrapperWriter(object):
     """
-    A class that creates a c file that wrapps the call to a specific function inside a shared object
+    A class that creates a c or c++ file that wrapps the call to a specific function inside a shared object
     """
-    def __init__(self, target_library):
+    def __init__(self, target_library, path_to_working_dir, language='c'):
         """
         :param target_library: path to the library to wrap
+        :param path_to_working_dir: path to the directory where sources are generated and compiled
+        :param language: chosen language (should be the same that the one used to build the target library)
         :type target_library: str
+        :type path_to_working_dir: str
+        :type language: str ('c'|'cpp'|'c++')
         """
         self._target_library = target_library
+        if not language in ['c', 'cpp', 'c++']:
+            raise ValueError("Available languages are C ('c') or C++ ('cpp'|'c++')")
+        if (os.path.isdir(path_to_working_dir)):
+            self._path_to_working_dir = path_to_working_dir
+        else:
+            raise IOError("The path {:s} is not a directory!".format(path_to_working_dir))
+        self._language = language
+        #
+        if self._language == 'c':
+            self._src_filename = os.path.splitext(os.path.basename(self._target_library))[0] + "_wrapper.c"
+        elif self._language in ['cpp', 'c++']:
+            self._src_filename = os.path.splitext(os.path.basename(self._target_library))[0] + "_wrapper.cpp"
+        self._src_filepath = os.path.join(self._path_to_working_dir, self._src_filename)
+        
     
-    def writeCFile(self, function_symbol = "undefined", function_signature = "undefined", opt_includes = None):
+    def writeCFile(self, function_symbol, function_signature, opt_includes = None):
         """
         Write c file wrapper using jinja and template
         
@@ -52,7 +71,37 @@ class FunctionWrapperWriter(object):
                            'target_symbol': function_symbol,
                            'func_params': func_params,
                            'func_params_names': ", ".join(func_params_names)}
-        print template.render(template_values)
+        with open(self._src_filepath, 'w') as fo:
+            fo.write(template.render(template_values))
+            
+    def compileSrcFile(self):
+        """
+        Compile the src file into a shared object that will wrap the call to the function
+        of the original library
+        """
+        shared_object_name = os.path.splitext(self._src_filename)[0] + ".so"
+        if self._language == 'c':
+            cmd = "gcc -D_GNU_SOURCE  -g -Wall -shared -fPIC -ldl {:s} -o {:s}".format(self._src_filename,
+                                                                                       shared_object_name)
+        else:
+            cmd = "g++ -D_GNU_SOURCE  -g -Wall -shared -fPIC -ldl {:s} -o {:s}".format(self._src_filename,
+                                                                                       shared_object_name)
+        saved_dir = os.getcwd()
+        os.chdir(self._path_to_working_dir);
+        try:
+            print >> stderr, "Launching command :\n{:s}".format(cmd)
+            subprocess.check_output(cmd, shell=True)
+        except subprocess.CalledProcessError as sub_error:
+            print >> stderr, "Problem encountered when executing command : {:s}".format(cmd)
+            print >> stderr, "Return code : {:d}".format(sub_error.returncode)
+            print >> stderr, "Available output : {:s}".format(sub_error.output)
+        finally:
+            os.chdir(saved_dir)
+        print "Compilation terminated successfully"
+        print "To launch your program with the wrapper library intercepting original one type :"
+        ld_preload = os.path.join(self._path_to_working_dir, shared_object_name)
+        print "LD_PRELOAD={:s} /path/to/your/program".format(ld_preload)
+        print "Bye!"
 
 
 def splitFunctionPrototype(func_prototype):
@@ -86,9 +135,10 @@ if __name__ == "__main__":
     target_signature = ("""void solveVolumeEnergy(MieGruneisenParameters_t *params, const double specific_volume,"""
                         """ const double internal_energy, double* pressure, double* gamma_per_vol, double* c_son)""")
     my_func_wrap = FunctionWrapperWriter("""/home/guillaume2/Documents/Informatique/WORKSPACE_ECLIPSE/XVOF_NP/xvof/"""
-                                         """element/vnr_internal_energy_evolution.so""")
+                                         """element/vnr_internal_energy_evolution.so""", "/tmp/test")
     my_func_wrap.writeCFile(function_symbol="solveVolumeEnergy",
                             function_signature="""void solveVolumeEnergy(MieGruneisenParameters_t *params, const double specific_volume,"""
                                                """ const double internal_energy, double* pressure, double* gamma_per_vol, double* c_son)""",
                             opt_includes="""/home/guillaume2/Documents/Informatique/WORKSPACE_ECLIPSE/"""
                         """nonlinear_solver/Newton/miegruneisen.h""")
+    my_func_wrap.compileSrcFile()
