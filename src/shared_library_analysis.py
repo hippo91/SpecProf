0484a8d8e0b3be68001982c019b5a0f20057c1c5
guillaume2 <guillaume.peillex@gmail.col>
2016-09-25 21:03:33 +0200

    Use of colored looger. Add of the method _determine_language to detect if the target library is a C or C++ one

--- src/shared_library_analysis.py
+++ src/shared_library_analysis.py
@@ -1,12 +1,14 @@
-# -*- coding: iso-8859-1 -*-
 """
 A module implementing the SharedObjectAnalyser class
 """
 import os
 import logging
 import subprocess
+from colored_logger import ColoredLoggerAdapter
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("SpecProf.shared_library_analysis")
+logger.setLevel(logging.DEBUG)
+adapter = ColoredLoggerAdapter(logger)
 
 
 class SharedObjectAnalyser(object):
@@ -18,8 +20,13 @@ class SharedObjectAnalyser(object):
         :param path_to_so: path to the shared object
         :type path_to_so: str
         """
-        self._path_to_so = self._check_path(os.path.expanduser(path_to_so))
-        self._symbols = self._get_symbol_names()
+        self.__path_to_so = self._check_path(os.path.expanduser(path_to_so))
+        self.__symbols = self._get_symbol_names()
+        self.__language = self._determine_language()
+
+    @property
+    def language(self):
+        return self.__language
 
     @staticmethod
     def _check_path(path):
@@ -33,7 +40,7 @@ class SharedObjectAnalyser(object):
                 - if it is not a regular file
                 - if file extension is not .so  
         """
-        logger.info("Checking path ...")
+        adapter.info("Checking path : {:s} ...".format(path))
         if not os.path.exists(path):
             msg = "The path {:s} is not an existing file!".format(path)
         elif not os.path.isfile(path):
@@ -41,9 +48,9 @@ class SharedObjectAnalyser(object):
         elif not os.path.splitext(path)[1] == ".so":
             msg = "The file under {:s} doesn't seem to be a shared object (.so file)".format(path)
         else:
-            logger.info("Path seems to correspond to a shared object")
+            adapter.info("Path seems to correspond to a shared object")
             return path
-        logger.error(msg)
+        adapter.error(msg)
         raise IOError(msg)
     
     def _get_symbol_names(self):
@@ -53,16 +60,16 @@ class SharedObjectAnalyser(object):
         :return: the list of symbols found in the shared object
         :rtype: list
         """
-        logger.info("Trying to get symbol names for the shared object...")
-        cmd = "nm {:s}".format(self._path_to_so)
+        adapter.info("Trying to get symbol names for the shared object...")
+        cmd = "nm {:s}".format(self.__path_to_so)
         try:
-            logger.info("Call of command : %s", cmd)
+            adapter.info("Call of command : {:s}".format(cmd))
             output_bytes = subprocess.check_output(cmd, shell=True)
         except subprocess.CalledProcessError as error:
-            logging.error("The command %s doesn't succeed! Return code is : %d", cmd, error.returncode)
-            logging.error("%s", error.output)
+            adapter.error("The command {:s} doesn't succeed! Return code is : {:d}".format(cmd, error.returncode))
+            adapter.error("{:s}".format(error.output))
             raise
-        logger.info("Command succeed!")
+        adapter.info("Command succeed!")
         output_str = output_bytes.decode("iso-8859-1").split(os.linesep)
         return [line.split(' ')[-1] for line in output_str]
 
@@ -73,12 +80,41 @@ class SharedObjectAnalyser(object):
         :param func_name: name of the function to monitor
         :type func_name: str 
         """
-        potential_targets = [symbol for symbol in self._symbols if symbol.find(func_name) != -1]
-        target = raw_input("""Among all the following symbols which one is of interests?"""
-                           """{:s}{:s}{:s}""".format(os.linesep, ":".join(potential_targets), os.linesep))
+        potential_targets = [symbol for symbol in self.__symbols if symbol.find(func_name) != -1]
+        if len(potential_targets) != 1:
+            target = raw_input("""Among all the following symbols which one is of interests?"""
+                               """{:s}{:s}{:s}""".format(os.linesep, ", ".join(potential_targets), os.linesep))
+        else:
+            target = potential_targets[0]
         if len(target.split()) != 1:
+            adapter.error("Please select only one symbol!")
             raise ValueError("Please select only one symbol!")
         return target
+
+    def _determine_language(self):
+        """
+        Try to determine if the library is a C or C++ one
+
+        :return: the language used to build the shared library ('c'|'c++')
+        """
+        adapter.info("Trying to determine the language (c or c++) of the shared object...")
+        cmd = "ldd {:s}".format(self.__path_to_so)
+        try:
+            adapter.info("Call of command : {:s}".format(cmd))
+            output_bytes = subprocess.check_output(cmd, shell=True)
+        except subprocess.CalledProcessError as error:
+            adapter.error("The command {:s} doesn't succeed! Return code is : {:d}".format(cmd, error.returncode))
+            adapter.error("{:s}".format(error.output))
+            raise
+        adapter.info("Command succeed!")
+        output_str = output_bytes.decode("iso-8859-1").split(os.linesep)
+        for lib in output_str:
+            lib = lib.lstrip()
+            if lib.startswith('libstdc++'):
+                adapter.info("The shared library seems to be a C++ one!")
+                return 'c++'
+        adapter.info("The shared library seems to be a C one!")
+        return 'c'
         
         
 if __name__ == "__main__":
