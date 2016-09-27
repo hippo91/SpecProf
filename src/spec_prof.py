85e3f696c6d797128f1ee804ed1243a8a5ff4723
guillaume.peillex <guillaume.peillex@gmail.com>
2016-09-27 09:21:31 -0400

    _epilog() + README enhancements

--- src/spec_prof.py
+++ src/spec_prof.py
@@ -81,34 +81,34 @@ def _epilog():
 
     # Usage
 
-    We want to profile the function named *computePressure* in the shared library under the path /path/to/libcompute_hydrodynamics.so. The complete signature
+    We want to profile the function named **computePressure** in the shared library under the path **/path/to/libcompute_hydrodynamics.so**. The complete signature
     of the function is :
 
     **void computePressure(const MySpecialObject&, double* datas, int param);**
 
     We just have to run :
 
-    ./spec_prof.py -o /path/to/libcompute_hydrodynamics.so -s "void computePressure(const MySpecialObject&, double* datas, int param)" -w /tmp/working_dir -i /path/to/header/my_special_object.h
+    `./spec_prof.py -o /path/to/libcompute_hydrodynamics.so -s "void computePressure(const MySpecialObject&, double* datas, int param)" -w /tmp/working_dir -i /path/to/header/my_special_object.h`
 
-    -o : Indicates path to the target library (mandatory)
+    **-o** : Indicates path to the target library (mandatory)
 
-    -s : The signature of the function or method to profile (mandatory)
+    **-s** : The signature of the function or method to profile (mandatory)
 
-    -w : path to the directory where spec_prof will generate the source file and the shared library wrapper (mandatory)
+    **-w** : path to the directory where spec_prof will generate the source file and the shared library wrapper (mandatory)
 
-    -i : path to header files containing the definition of the objects types appearing in the function signature (optional)
-    
-    SpecProf will generate a file named libcompute_hydrodynamics_wrapper.cpp and a shared object libcompute_hydrodynamics_wrapper.so in the /tmp/working_dir directory. 
+    **-i** : path to header files containing the definition of the objects types appearing in the function signature (optional)
+
+    SpecProf will generate a file named **libcompute_hydrodynamics_wrapper.cpp** and a shared object **libcompute_hydrodynamics_wrapper.so** in the /tmp/working_dir directory. 
 
     # Postscript
 
     Once generated, the shared library wrapper is used thanks to the following command :
 
-    LD_PRELOAD=/tmp/working_dir/libcompute_hydrodynamics_wrapper.so /path/to/executable/using/the/target/library
-    
+    `LD_PRELOAD=/tmp/working_dir/libcompute_hydrodynamics_wrapper.so /path/to/executable/using/the/target/library`
+
     # Prerequisites
-    
-    Standard unix/linux tools such as *ldd* *nm* and a compilator able to deal with C++2011 are required.
+
+    Standard unix/linux tools such as **ldd** **nm** and a compilator able to deal with C++2011 are required.
     """
     return msg
 
