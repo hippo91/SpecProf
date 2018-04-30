f296c2d35af7efb82fce380fb85f6618bb569a64
hippo91 <guillaume.peillex@gmail.com>
2018-04-30 14:49:36 +0200

    The function signature is no more an input of the program but is instead obtained via c++filt and user interaction + pylint

--- src/shared_library_analysis.py
+++ src/shared_library_analysis.py
@@ -1,14 +1,38 @@
 """
 A module implementing the SharedObjectAnalyser class
 """
+from __future__ import print_function
 import os
 import logging
 import subprocess
-from colored_logger import ColoredLoggerAdapter
+from src.colored_logger import ColoredLoggerAdapter
 
-logger = logging.getLogger("SpecProf.shared_library_analysis")
-logger.setLevel(logging.DEBUG)
-adapter = ColoredLoggerAdapter(logger)
+LOGGER = logging.getLogger("SpecProf.shared_library_analysis")
+LOGGER.setLevel(logging.DEBUG)
+ADAPTER = ColoredLoggerAdapter(LOGGER)
+
+
+def _exec_and_log(cmd, encoding='iso-8859-1'):
+    """
+    Execute the command in a subprocess and log the error or the success
+
+    :param cmd: command to execute
+    :type cmd: str
+    :param encoding: encoding of the output string
+    :type encoding: str
+    :return: the result of the command
+    :rtype: str
+    """
+    ADAPTER.info("Call of command : < {:s} >".format(cmd))
+    try:
+        output_bytes = subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError as error:
+        ADAPTER.error("""The command {:s} doesn't succeed!"""
+                      """ Return code is : {:d}""".format(cmd, error.returncode))
+        ADAPTER.error("{:s}".format(error.output))
+        raise
+    ADAPTER.info("Command succeed!")
+    return output_bytes.decode(encoding).split(os.linesep)
 
 
 class SharedObjectAnalyser(object):
@@ -23,24 +47,31 @@ class SharedObjectAnalyser(object):
         self.__path_to_so = self._check_path(os.path.expanduser(path_to_so))
         self.__symbols = self._get_symbol_names()
         self.__language = self._determine_language()
+        self.__mangling_map = self._get_symbol_unmangled_map()
 
     @property
     def language(self):
+        """
+        Language pty accessor
+
+        :return: language of the library
+        :rtype: str
+        """
         return self.__language
 
     @staticmethod
     def _check_path(path):
         """
         Check that the path given corresponds to an existing file with '.so' extension
-        
+
         :param path: path to test
         :type path: str
-        :raise IOError: 
+        :raise IOError:
                 - if the path doesn't exist
                 - if it is not a regular file
-                - if file extension is not .so  
+                - if file extension is not .so
         """
-        adapter.info("Checking path : {:s} ...".format(path))
+        ADAPTER.info("Checking path : {:s} ...".format(path))
         if not os.path.exists(path):
             msg = "The path {:s} is not an existing file!".format(path)
         elif not os.path.isfile(path):
@@ -48,63 +79,63 @@ class SharedObjectAnalyser(object):
         elif not os.path.splitext(path)[1] == ".so":
             msg = "The file under {:s} doesn't seem to be a shared object (.so file)".format(path)
         else:
-            adapter.info("Path seems to correspond to a shared object")
+            ADAPTER.info("Path seems to correspond to a shared object")
             return path
-        adapter.error(msg)
+        ADAPTER.error(msg)
         raise IOError(msg)
-    
+
+    def _get_symbol_unmangled_map(self):
+        """
+        Return a mapping between mangled and unmangled symbols in the shared
+        object libraray
+
+        :return: the mapping between mangled and unmangled symbols
+        :rtype: dict
+        """
+        ADAPTER.info("Trying to compute mapping between mangled and unmangled symbols "
+                     "in the shared object")
+        res = {}
+        for _sym in self.__symbols:
+            if _sym and self.__language == "c++":
+                cmd = "c++filt -it {:s}".format(_sym)
+                output_str = _exec_and_log(cmd)
+                if output_str[0] != _sym:
+                    res[_sym] = output_str[0]
+            else:
+                res[_sym] = _sym
+        return res
+
     def _get_symbol_names(self):
         """
         Get the symbol name in the shared object thanks to nm utility
-        
+
         :return: the list of symbols found in the shared object
         :rtype: list
         """
-        adapter.info("Trying to get symbol names for the shared object...")
+        ADAPTER.info("Trying to get symbol names for the shared object...")
         cmd = "nm {:s}".format(self.__path_to_so)
-        try:
-            adapter.info("Call of command : {:s}".format(cmd))
-            output_bytes = subprocess.check_output(cmd, shell=True)
-        except subprocess.CalledProcessError as error:
-            adapter.error("The command {:s} doesn't succeed! Return code is : {:d}".format(cmd, error.returncode))
-            adapter.error("{:s}".format(error.output))
-            raise
-        adapter.info("Command succeed!")
-        output_str = output_bytes.decode("iso-8859-1").split(os.linesep)
+        output_str = _exec_and_log(cmd)
         return [line.split(' ')[-1] for line in output_str]
 
-    def ask_for_symbol(self, func_name, class_name=None, namespace=None):
+    def print_mangling_map(self):
         """
-        Ask the user which symbol is relevant in all symbols matching the function name
-        
-        :param func_name: name of the function to monitor
-        :type func_name: str 
+        Print the mapping of mangled and unmangled symbol on sys.stdout
         """
-        func_name_targets = {symbol for symbol in self.__symbols if symbol.find(func_name) != -1}
-        class_name_targets = set(func_name_targets)
-        namespace_targets = set(func_name_targets)
-        if class_name is not None:
-            class_name_targets = {symbol for symbol in self.__symbols if symbol.find(class_name) != -1}
-        if namespace is not None:
-            namespace_targets = {symbol for symbol in self.__symbols if symbol.find(namespace) != -1}
-        potential_targets = func_name_targets & class_name_targets & namespace_targets
-        if len(potential_targets) == 0:
-            msg = "Unable to detect a symbol corresponding to the function!"
-            adapter.error(msg)
-            adapter.error("Targets of the function name are : {:s}".format(func_name_targets))
-            adapter.error("Targets of the class name are : {:s}".format(class_name_targets))
-            adapter.error("Targets of the namespace are : {:s}".format(namespace_targets))
-            adapter.error("Intersection of the three sets is nill!")
-            raise ValueError(msg)
-        if len(potential_targets) != 1:
-            target = raw_input("""Among all the following symbols which one is of interests?"""
-                               """{:s}{:s}{:s}""".format(os.linesep, ", ".join(potential_targets), os.linesep))
-        else:
-            target = potential_targets.pop()
-        if len(target.split()) != 1:
-            adapter.error("Please select only one symbol!")
-            raise ValueError("Please select only one symbol!")
-        return target
+        for key, value in self.__mangling_map.items():
+            print("{:s} <==> {:s}".format(key, value))
+
+    def ask_for_symbol(self):
+        """
+        Ask the user which symbol is relevant.
+
+        :return: a triplet symbol/unmangled name of the symbol/return type
+        :rtype: (symbol, unmangled_name, return type)
+        """
+        self.print_mangling_map()
+        answer = raw_input("""Among all the preceeding symbols which one if of interest?"""
+                           + os.linesep)
+        rtype = raw_input("""For the selected symbol what is the associated return type?""")
+        return (answer, self.__mangling_map[answer], rtype)
 
     def _determine_language(self):
         """
@@ -112,26 +143,18 @@ class SharedObjectAnalyser(object):
 
         :return: the language used to build the shared library ('c'|'c++')
         """
-        adapter.info("Trying to determine the language (c or c++) of the shared object...")
+        ADAPTER.info("Trying to determine the language (c or c++) of the shared object...")
         cmd = "ldd {:s}".format(self.__path_to_so)
-        try:
-            adapter.info("Call of command : {:s}".format(cmd))
-            output_bytes = subprocess.check_output(cmd, shell=True)
-        except subprocess.CalledProcessError as error:
-            adapter.error("The command {:s} doesn't succeed! Return code is : {:d}".format(cmd, error.returncode))
-            adapter.error("{:s}".format(error.output))
-            raise
-        adapter.info("Command succeed!")
-        output_str = output_bytes.decode("iso-8859-1").split(os.linesep)
+        output_str = _exec_and_log(cmd)
         for lib in output_str:
             lib = lib.lstrip()
             if lib.startswith('libstdc++'):
-                adapter.info("The shared library seems to be a C++ one!")
+                ADAPTER.info("The shared library seems to be a C++ one!")
                 return 'c++'
-        adapter.info("The shared library seems to be a C one!")
+        ADAPTER.info("The shared library seems to be a C one!")
         return 'c'
-        
-        
+
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s:%(message)s', level=logging.INFO)
     my_analyser = SharedObjectAnalyser("""~/Documents/Informatique/WORKSPACE_ECLIPSE/"""
